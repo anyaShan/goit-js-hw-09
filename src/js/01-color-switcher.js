45c5c1c8932fa0c1a095832f58d9160f4640dfbc
anya.shan <anya.shanaida@yahoo.com>
2022-08-16 20:30:31 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyEl: document.body,
+  btnEl: document.querySelectorAll('button'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.bodyEl.style.textAlign = 'center';
+
+let timerId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
