623c79949c7f2a76447b40dc03393ea5bc15d46e
anya.shan <anya.shanaida@yahoo.com>
2022-08-17 22:44:09 +0300

    task-03 correction

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,21 @@
 const refs = {
   formEl: document.querySelector('.form'),
-  delayInputEl: document.querySelector('input[name="delay"]'),
-  stepInputEl: document.querySelector('input[name="step"]'),
-  amountInputEl: document.querySelector('input[name="amount"]'),
   submitBtnEl: document.querySelector('button[type="submit"]'),
 };
 
-refs.submitBtnEl.addEventListener('submit', onFormSubmit);
+const body = document.body;
+body.classList = 'container';
 
-function onFormSubmit(event) {
-  const firstDelay = Number(refs.delayInputEl);
-  const delay = Number(refs.stepInputEl);
-  setTimeout(() => {
-    const positions = Number(refs.amountInputEl);
-    for (let position = 1; position <= positions; position += 1) {
-      createPromise(position, delay);
-    }
-  }, firstDelay);
-}
+const listOfPromises = document.createElement('ul');
+listOfPromises.classList = 'promises-list';
+refs.formEl.after(listOfPromises);
+
+refs.formEl.addEventListener('submit', onFormSubmit);
 
 function createPromise(position, delay) {
-  // delay = Number(refs.stepInputEl);
-
   return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
@@ -34,22 +25,38 @@ function createPromise(position, delay) {
   });
 }
 
-createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    fulfilled({ position, delay });
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    rejected({ position, delay });
-  });
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let firstDelay = Number(delay.value);
+  let stepEl = Number(step.value);
+  let amountEl = Number(amount.value);
+
+  console.log(firstDelay, stepEl, amountEl);
+
+  for (let position = 1; position <= amountEl; position += 1) {
+    createPromise(position, firstDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        fulfilled({ position, delay });
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        rejected({ position, delay });
+      });
+    firstDelay += stepEl;
+  }
+}
 
 function fulfilled(position, delay) {
-  const string = `<p class="fulfilled-text">✅ Fulfilled promise ${position} in ${delay}ms</p>`;
-  refs.formEl.insertAdjacentHTML('beforeend', string);
+  const string = `<li class="promises-item fulfilled-text">✅ Fulfilled promise ${position} in ${delay}ms</li>`;
+  listOfPromises.insertAdjacentHTML('beforeend', string);
 }
 
 function rejected(position, delay) {
-  const string = `<p class="rejected-text">❌ Rejected promise ${position} in ${delay}ms</p>`;
-  refs.formEl.insertAdjacentHTML('beforeend', string);
+  const string = `<li class="promises-item rejected-text">❌ Rejected promise ${position} in ${delay}ms</li>`;
+  listOfPromises.insertAdjacentHTML('beforeend', string);
 }
