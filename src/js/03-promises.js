2d33111fe5b908bb2e9105731d670e045edc6330
anya.shan <anya.shanaida@yahoo.com>
2022-08-18 17:12:57 +0300

    task-03 add Notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -77,3 +77,61 @@ function rejected({ position, delay }) {
   const string = `<li class="promises-item rejected-text">❌ Rejected promise ${position} in ${delay}ms</li>`;
   refs.listOfPromises.insertAdjacentHTML('beforeend', string);
 }
+
+// =================================VERSION 2==================================================
+
+// import Notiflix from 'notiflix';
+
+// const refs = {
+//   formEl: document.querySelector('.form'),
+//   submitBtnEl: document.querySelector('button[type="submit"]'),
+// };
+
+// refs.formEl.addEventListener('submit', onFormSubmit);
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { delay, step, amount },
+//   } = event.currentTarget;
+
+//   let firstDelay = Number(delay.value);
+//   let stepEl = Number(step.value);
+//   let amountEl = Number(amount.value);
+
+//   if (firstDelay < 0 || stepEl < 0 || amountEl < 0) {
+//     alert('Enter a number greater than 0');
+//     return;
+//   }
+
+//   console.log(firstDelay, stepEl, amountEl);
+
+//   for (let position = 1; position <= amountEl; position += 1) {
+//     createPromise(position, firstDelay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(
+//           `❌ Rejected promise ${position} in ${delay}ms`
+//         );
+//       });
+//     firstDelay += stepEl;
+//   }
+// }
