9d585657411f1bdeb0eb8a5a46dd2fa4370687c5
anya.shan <anya.shanaida@yahoo.com>
2022-08-18 23:49:46 +0300

    task-02 add Notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   inputDateEl: document.querySelector('#datetime-picker'),
@@ -25,7 +26,7 @@ flatpickr(refs.inputDateEl, {
     let futureTime = selectedDates[0].getTime();
 
     if (futureTime < Date.now()) {
-      alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
     }
     refs.startBtnEl.disabled = false;
 
